17705c80b6c444d03783eb1f4182060d7162db39
Maisy Dodge <dodgemai@grinnell.edu>
2019-06-27 11:10:51 -0500

    javascript practice added

---
+++ JavascriptPractice/displayMousePosition.js
@@ -0,0 +1,29 @@
+var isMouseTracking = false;
+
+function updateMousePosition(){
+  // If tracking is enabled, update the labels
+    if (isMouseTracking) {
+        // We can get the mouse positions with
+        // event.clientX and event.clientY.
+        // We can update the text of an HTML element with the
+        // innerText property.
+        var positionX = document.getElementById("mousePositionX");
+        positionX.innerText = event.clientX;
+        var positionY = document.getElementById("mousePositionY");
+        positionY.innerText = event.clientY;
+    }
+}
+
+function toggleTracking(){
+    // If isMouseTracking is true, set it to false
+    // Otherwise it is false, and needs to be set to true
+    isMouseTracking = !isMouseTracking;
+    // Update the tracking status to show whether it is enabled
+    if (isMouseTracking) {
+        document.getElementById("trackingStatus")
+            .innerText = "TRACKING";
+    } else {
+        document.getElementById("trackingStatus")
+            .innerText = "NOT TRACKING";
+    }
+}
